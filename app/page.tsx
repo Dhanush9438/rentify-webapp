e4f5e321433a1d726eca22bf1f3b738e9c67982c
dhanush <111906633+Dhanush9438@users.noreply.github.com>
2024-05-26 17:29:51 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import { LoginButton } from "@/components/login-button";
 import { Button } from "@/components/ui/button";
 import Navbar from "@/components/navbar";
-import { connectDB } from '@/lib/connectDB'
+
 export default function Home() {
-   await connectDB();
-    const Data = await House.find()
   return (
-    <div>
-        <Navbar/>
-    <div><Filter houseData={Data}/></div>
-    </div>
-  )
+    <main>
+      <Navbar/>
+    </main>
+  );
 }
